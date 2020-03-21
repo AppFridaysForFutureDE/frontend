a5059ddbe32345c607ad5d43246d8023ab3485f8
redsolver <info@redsolver.net>
2020-03-21 12:10:45 +0100

    Dark Mode Support

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 export 'package:flutter/material.dart';
 export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+export 'package:dynamic_theme/dynamic_theme.dart';
 
 export 'package:app/model/og.dart';
 
