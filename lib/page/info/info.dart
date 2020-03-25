201c9c3035dd50cfbcb4488724db5cf65b25b792
Joris <jjoris@web.de>
2020-03-25 17:54:11 +0100

    Only added a few Comments and Stuff

--- lib/page/info/info.dart
+++ lib/page/info/info.dart
@@ -11,13 +11,16 @@ class _InfoPageState extends State<InfoPage> {
   List<OG> ogL;
   List<OgTileSave> ogSaveL;
   _InfoPageState(){
+    //Dummy Data
     ogL = [
       _cOG('123','Kieler OG 1','Kiel','Schleswig Holstein', 22223344344,7766767877,'Pleeeenumm ist wichtig für ...', 'https://facebook.com','https://instagram.com','https://twitter.com/','https://fridaysforfuture.de'),
       _cOG('123566','Kieler OG 2','Kiel','Schleswig Holstein', 22223344344,7766767877,'Pleeeenumm ist wichtig für ...', 'https://facebook.com','https://instagram.com','twitter.com/','https://fridaysforfuture.de'),
       _cOG('123566','München zu faul zum eintragen',null,'Bayern', 22223344344,7766767877,null, 'https://facebook.com',null,'https://twitter.com','https://fridaysforfuture.de'),
     ];
+    // Maps the data of the OG into a OgTileSave which represents a OgTile without have a widget loaded
     ogSaveL = ogL.map((og) { return OgTileSave(og);}).toList();
   }
+  //Only to create the Dummy data
   static OG _cOG(String id, String name, String stadt, String bundesland, num long, num lat, String zusatzinfo,  String facebook,String instagram,String twitter,String website){
     OG og = OG();
     og.id = id;
@@ -37,6 +40,7 @@ class _InfoPageState extends State<InfoPage> {
     og.website = website;
     return og;
   }
+  //the main Build function
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,14 +48,14 @@ class _InfoPageState extends State<InfoPage> {
         title: Text('Ortsgruppen Infos'),
       ),
       body: Center(
-        child: ListView.separated(
+        child: ListView.separated( // the seperator of the Croll view
           padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
 
-          separatorBuilder: (context, index) => Divider(
+          separatorBuilder: (context, index) => Divider(// the seperator of the Croll view
             color: Colors.black,
           ),
           itemCount: ogSaveL.length,
-          itemBuilder: (context, index) => OgTile(ogSaveL[index],setState),
+          itemBuilder: (context, index) => OgTile(ogSaveL[index],setState), // creates a ogTile from a OgTileSave
 
         )
       ),
--- lib/widget/og_tile.dart
+++ lib/widget/og_tile.dart
@@ -1,14 +1,22 @@
 import 'package:url_launcher/url_launcher.dart';
 
 import '../app.dart';
-
+/*
+A Tile wich displays a OG
+ */
 class OgTile extends StatelessWidget{
   OgTileSave ogSave;
   OG og;
   Function delegateSetState;
+  /*
+  The Contructor take a OgTileSave and the setState Method of the Delegate
+   */
   OgTile(this.ogSave, this.delegateSetState){
     og = ogSave.og;
   }
+  /*
+  Launches a URl or throws an Error
+   */
   _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
@@ -16,6 +24,10 @@ class OgTile extends StatelessWidget{
       throw 'Could not launch $url';
     }
   }
+  /*
+  Takes a icon and a url String
+  and Builds a Row with a Icon and a social Media Link
+   */
   Widget _buildSocialMedia(IconData icon,String url){
     return (url == null || url == '')
         ? Container()
@@ -31,6 +43,10 @@ class OgTile extends StatelessWidget{
       ],
     );
   }
+  /*
+  Builds the Location Text Widgets
+  Depending on wich filds are set
+   */
   Widget _buildLocation(){
     if(og.stadt == null || og.stadt == ''){
       if(og.bundesland == null || og.bundesland == ''){
@@ -43,9 +59,12 @@ class OgTile extends StatelessWidget{
     }
     return Text(og.stadt+' - '+og.bundesland);
   }
+  /*
+  The Main Build Method
+   */
   @override
   Widget build(BuildContext context){
-    return ogSave.tapped ?
+    return ogSave.tapped ? //If Activated show extended View of the og
         Container(
 
           padding: EdgeInsets.fromLTRB(0.0,15.0,10.0,0.0),
@@ -72,15 +91,7 @@ class OgTile extends StatelessWidget{
             ),
           ),
         )
-    /*ListTile( //expanded version
-        title: Text(ogSave.og.name),
-        subtitle: Text(ogSave.og.stadt+ ' - '+ogSave.og.bundesland+'\n'+ogSave.og.zusatzinfo +'\n'+'\n'+ogSave.og.facebook),
-        isThreeLine: true,
-        onTap: (){delegateSetState((){
-          ogSave.tapped = !ogSave.tapped;
-        });},
-    )*/
-        : ListTile(
+        : ListTile( // Else shows if not Activated
       title: Text(ogSave.og.name),
       onTap: (){delegateSetState((){
         ogSave.tapped = !ogSave.tapped;
@@ -89,6 +100,9 @@ class OgTile extends StatelessWidget{
   }
 }
 
+/*
+A class wich represent a Tile without the need of have one loaded
+ */
 class OgTileSave{
   OG og;
   bool tapped = false;
