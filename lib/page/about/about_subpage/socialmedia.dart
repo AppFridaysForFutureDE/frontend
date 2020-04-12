1d10b2734365c0ed1c85e3779e7bcdd784afed69
Nic <61389509+craeosh@users.noreply.github.com>
2020-04-12 21:14:16 +0200

    Social Media Icons

--- lib/page/about/about_subpage/socialmedia.dart
+++ lib/page/about/about_subpage/socialmedia.dart
@@ -22,36 +22,42 @@ class SocialMediaPage extends  StatelessWidget {
         children: <Widget>[
           TitleWidget('Social Media Kanäle'),
           ListTile(
+            leading: Icon(MdiIcons.twitter, /*color: Color(0x387245)*/), //Bei anderen Farben verschwindet das Icon
             title: Text('Twitter'),
             onTap: () {
                 _launchURL('https://twitter.com/FridayForFuture');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.instagram),
             title: Text('Instagram'),
             onTap: () {
                 _launchURL('https://www.instagram.com/fridaysforfuture.de');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.facebook),
             title: Text('Facebook'),
             onTap: () {
                 _launchURL('https://www.facebook.com/fridaysforfuture.de/');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.flickr),
             title: Text('Flickr'),
             onTap: () {
                 _launchURL('https://www.flickr.com/people/146245435@N02/');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.youtube),
             title: Text('YouTube'),
             onTap: () {
                 _launchURL('https://www.youtube.com/channel/UCZwF7J5rbyJXBZMJrE_8XCA');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.telegram),
             title: Text('Telegram'),
             onTap: () {
                 _launchURL('https://t.me/FridaysForFutureDE');
@@ -59,18 +65,21 @@ class SocialMediaPage extends  StatelessWidget {
           ),
           TitleWidget('Diskussions- / Gruppen'),
           ListTile(
+            leading: Icon(MdiIcons.whatsapp),
             title: Text('Whats App'),
             onTap: () {
                 _launchURL('https://chat.whatsapp.com/DlXVuh9KXx10B5LXi7MvsL');
               },
           ),
           ListTile(
+            leading: Icon(MdiIcons.telegram),
             title: Text('Telegram'),
             onTap: () {
                 _launchURL('https://t.me/fffDiskussion');
               },
           ), 
           ListTile(
+            leading: Icon(MdiIcons.discord),
             title: Text('Discord'),
             onTap: () {
                 _launchURL('https://discord.gg/UEGyyrT');
