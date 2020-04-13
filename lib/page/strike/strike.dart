1949289f73d43a076437edc41830754e0768c385
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-13 11:04:39 +0200

    add card placeholders

--- lib/page/strike/strike.dart
+++ lib/page/strike/strike.dart
@@ -7,10 +7,35 @@ class StrikePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Netzstreik'),
       ),
-      body: Center(
-        child: Text(
-          'Hier kommen Tolle Streiks hin',
-        ),
+      body: ListView(
+        children: <Widget>[
+          _buildCard('Online Streik', 'Jetzt Foto hochladen und mitmachen!'),
+          _buildCard('Zukunft Online', 'Instagram'),
+          _buildCard('Challenge', 'Lust auf eine kleine Herausforderung? Mach mit!'),
+        ]
+      ),
+    );
+  }
+
+ Widget _buildCard(String title, String subtitle) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.album),
+            title: Text(title),
+            subtitle: Text(subtitle),
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: const Text('JETZT STARTEN'),
+                onPressed: () {/* ... */},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
