bd67ed9b9fd8f8ba9c337ce54aa88c7ffb83b241
redsolver <info@redsolver.net>
2020-04-13 13:07:53 +0200

    Flip Cards und Navigation

--- lib/page/strike/strike.dart
+++ lib/page/strike/strike.dart
@@ -1,5 +1,7 @@
 import 'package:app/app.dart';
 
+import 'package:flip_card/flip_card.dart';
+
 class StrikePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,35 +9,76 @@ class StrikePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Netzstreik'),
       ),
-      body: ListView(
-        children: <Widget>[
-          _buildCard('Online Streik', 'Jetzt Foto hochladen und mitmachen!'),
-          _buildCard('Zukunft Online', 'Instagram'),
-          _buildCard('Challenge', 'Lust auf eine kleine Herausforderung? Mach mit!'),
-        ]
-      ),
+      body: Column(children: <Widget>[
+        _buildCard(
+          'Online Streik',
+          'Jetzt Foto hochladen und mitmachen!',
+          () {
+            // Tu was
+          },
+        ),
+        _buildCard(
+          'Zukunft Online',
+          'Instagram',
+          () {
+            // Tu was
+          },
+        ),
+        _buildCard(
+          'Challenge',
+          'Lust auf eine kleine Herausforderung? Mach mit!',
+          () {
+            // Tu was
+          },
+        ),
+      ]),
     );
   }
 
- Widget _buildCard(String title, String subtitle) {
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.album),
-            title: Text(title),
-            subtitle: Text(subtitle),
-          ),
-          ButtonBar(
+  Widget _buildCard(String title, String subtitle, Function onClickStart) {
+    return Expanded(
+      child: FlipCard(
+        direction: FlipDirection.HORIZONTAL, // default
+        front: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              FlatButton(
-                child: const Text('JETZT STARTEN'),
-                onPressed: () {/* ... */},
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text(title),
+                subtitle: Text(subtitle),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('JETZT STARTEN'),
+                    onPressed: onClickStart,
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
+        back: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text(title),
+                subtitle: Text(subtitle),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('JETZT STARTEN'),
+                    onPressed: onClickStart,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
