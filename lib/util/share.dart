cf0b4f7362e3869024f06a50f763e6f3afb9b93c
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-13 19:55:22 +0200

    add todo

--- lib/util/share.dart
+++ lib/util/share.dart
@@ -5,6 +5,7 @@ import 'package:app/model/post.dart';
 class ShareUtil {
   static sharePost(Post post) {
     // TODO Teilen-Text ändern
+    // TODO Eventuell esys_flutter_share anstatt share verwenden (und dann share aus den dependencies entferenen)
     Share.share('Lies "${post.title}" jetzt in der neuen FFFApp! <link>');
   }
 }
