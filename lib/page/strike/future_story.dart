c0d3a93ae04404de66ebe9899f7e4c431a46c881
redsolver <info@redsolver.net>
2020-04-14 23:41:21 +0200

    Kleine Anpassungen

--- lib/page/strike/future_story.dart
+++ lib/page/strike/future_story.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:app/app.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
@@ -7,7 +8,7 @@ import 'package:permission_handler/permission_handler.dart';
 class FutureStoryPage extends StatelessWidget {
   _shareImage() async {
     if (await Permission.storage.request().isGranted) {
-      var res = await http.get('https://app.fffutu.re/instagram_story.jpg');
+      var res = await http.get('https://app.fffutu.re/img/instagram_story.jpg');
       await ImageGallerySaver.saveImage(res.bodyBytes);
       _showSnack('Die Vorlage ist jetzt in deiner Galerie gespeichert.');
     } else {
@@ -32,25 +33,34 @@ class FutureStoryPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Zukunfts Story'),
       ),
-      body: Container(
+      body: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
           children: <Widget>[
-            Text('Nach Corona willst du in eine lebenswerte Zukunft blicken? ' +
-                'Eine Zukunft ohne weitere Pandemien, extreme Wettervorkommen oder andere Folgen des Klimawandels?'),
-            Text(
-                "Dann mach mit und fordere deine Klimaschutzmaßnahmen von der Politik!"),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: AutoSizeText(
+                'Nach Corona willst du in eine lebenswerte Zukunft blicken? ' +
+                    'Eine Zukunft ohne weitere Pandemien, extreme Wettervorkommen oder andere Folgen des Klimawandels?' +
+                    '\n' +
+                    'Dann mach mit und fordere deine Klimaschutzmaßnahmen von der Politik!',
+                maxLines: 7,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Spacer(),
             Stack(
               alignment: AlignmentDirectional.bottomCenter,
               children: <Widget>[
-                Image(
-                  image: NetworkImage(
-                      'https://app.fffutu.re/instagram_instructions.jpg'),
+                Image.network(
+                    'https://app.fffutu.re/img/instagram_instructions.jpg'),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32.0),
+                  child: RaisedButton(
+                      color: Theme.of(context).primaryColor,
+                      onPressed: _shareImage,
+                      child: Text('Vorlage herunterladen')),
                 ),
-                RaisedButton(
-                    color: Theme.of(context).primaryColor,
-                    onPressed: _shareImage,
-                    child: Text('Vorlage herunterladen')),
               ],
             )
           ],
