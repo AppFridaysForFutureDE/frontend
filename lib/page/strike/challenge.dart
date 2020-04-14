55a9fdff69dcd2c9f582898cd573ef001854c5fe
redsolver <info@redsolver.net>
2020-04-15 01:38:39 +0200

    Bee

--- lib/page/strike/challenge.dart
+++ lib/page/strike/challenge.dart
@@ -14,30 +14,34 @@ class _State extends State<ChallengePage> {
       ),
       body: ListView(
         children: [
-          Text(
-              "Hier sind ein paar richtig coole Ideen, welche du von Zuhause aus erledigen kannst:"),
           _beeChallenge(
-              "Poste auf social Media alle Klimamaßnahmen, die du dir von der Politik wünschst."),
-          //_beeChallenge(    "Kreiere ein neues Streikschild mit einem außergewöhnlichen Spruch."),
+            'c1',
+            'Der Picasso in dir!',
+            'Überlege dir eine Kunstaktion, ob Bild, Knete, Basteln oder Lego mit der du dein Klimathema repräsentierst. Ob klimaneutrales Haus, demonstrierende Menschen ein Windrad oder eine Kuh, deiner Kreativität sind keine Grenzen gesetzt.',
+          ),
+          _beeChallenge(
+              'c2',
+              'Kreiere ein neues Streikschild mit einem außergewöhnlichen Spruch.',
+              'Bastel dir ein Streikschild oder ein Banner, oder nimm ein vorhandenes Banner und hänge es gut sichtbar an deinen Balkon oder stelle es vor die Tür! Lass dir dabei von deinen Eltern helfen! Wenn du Hilfe mit einem Spruch brauchst, check unser Social Media! What do we want? Climate Justice!'),
         ],
       ),
     );
   }
 
-  Widget _beeChallenge(String text) {
+  Widget _beeChallenge(String id, String title, String text) {
     return Row(
       children: <Widget>[
         Column(
           children: <Widget>[
             Padding(
               padding: EdgeInsets.all(8.0),
-              child:
-                  Image.asset('assets/images/bee.png', height: 60, width: 60),
+              child: Image.asset('assets/images/bee_grey.jpg',
+                  height: 60, width: 60),
             ),
             RaisedButton(
                 color: Theme.of(context).primaryColor,
                 onPressed: () {}, // TODO
-                child: Text("Abschließen")),
+                child: Text('Erledigt')),
           ],
         ),
         Text(text),
