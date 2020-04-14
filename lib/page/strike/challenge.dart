c615dd44bff23124d49854c8ded8e6ed31be9da6
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-15 00:32:06 +0200

    fix bee image

--- lib/page/strike/challenge.dart
+++ lib/page/strike/challenge.dart
@@ -18,22 +18,29 @@ class _State extends State<ChallengePage> {
               "Hier sind ein paar richtig coole Ideen, welche du von Zuhause aus erledigen kannst:"),
           _beeChallenge(
               "Poste auf social Media alle Klimamaßnahmen, die du dir von der Politik wünschst."),
-          _beeChallenge(
-              "Kreiere ein neues Streikschild mit einem außergewöhnlichen Spruch."),
+          //_beeChallenge(    "Kreiere ein neues Streikschild mit einem außergewöhnlichen Spruch."),
         ],
       ),
     );
   }
 
   Widget _beeChallenge(String text) {
-    return Stack(
+    return Row(
       children: <Widget>[
-        Image.asset('bee-1296273_640.png'),
+        Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child:
+                  Image.asset('assets/images/bee.png', height: 60, width: 60),
+            ),
+            RaisedButton(
+                color: Theme.of(context).primaryColor,
+                onPressed: () {}, // TODO
+                child: Text("Abschließen")),
+          ],
+        ),
         Text(text),
-        RaisedButton(
-            // color: Theme.of(context).primaryColor,
-            onPressed: null, // TODO
-            child: Text('I did it!')),
       ],
     );
   }
