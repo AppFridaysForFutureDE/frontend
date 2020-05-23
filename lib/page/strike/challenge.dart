5d961ce8f1988d67bdcce0240fa04e4820b811fa
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-05-23 22:01:59 +0200

    make bee challenge more flexible

--- lib/page/strike/challenge.dart
+++ lib/page/strike/challenge.dart
@@ -22,32 +22,32 @@ class _State extends State<ChallengePage> {
             _beeChallenge(//TODO lufthansa.lol
               'c7',
               'Lol die Lufthansa!',
-              'Alle Städte Berlins mit Solaranlagen bedenken? 11.056.511 Kitaplätze schaffen? 1.113.861.368 Liter Bier kaufen? Nein die Regierung möchte mit dem Steuergeld nicht der Gesellschaft helfen, sondern Lufthansa retten! Nicht mit uns! Teile über lufthansa.lol mit deinen Freunden, was du mit 9 Milliarden Euro für die Gesellschaft machen würdest!',
+              Text('Alle Städte Berlins mit Solaranlagen bedenken? 11.056.511 Kitaplätze schaffen? 1.113.861.368 Liter Bier kaufen? Nein die Regierung möchte mit dem Steuergeld nicht der Gesellschaft helfen, sondern Lufthansa retten! Nicht mit uns! Teile über lufthansa.lol mit deinen Freunden, was du mit 9 Milliarden Euro für die Gesellschaft machen würdest!'),
             ),
             _beeChallenge(
               'c8',
               'Ein Stern für Ölkonzerne',
-              'Öl-Konzerne zerstören unsere Zukunft! Lasst uns das deutlich machen, indem wir enttäuschte Bewertung auf Google hinterlassen. #oilmustfall. Nimm dir 30 Sekunden Zeit um nach den Büros zu suchen und sag ihnen mit uns zusammen deine Meinung!',
+              Text('Öl-Konzerne zerstören unsere Zukunft! Lasst uns das deutlich machen, indem wir enttäuschte Bewertung auf Google hinterlassen. #oilmustfall. Nimm dir 30 Sekunden Zeit um nach den Büros zu suchen und sag ihnen mit uns zusammen deine Meinung!'),
             ),
             _beeChallenge( //TODO LINK YT
               'c9',
               '#WirBildenZukunft',
-              'In der Schule wird nicht ausreichend über die Klimakrise unterrichtet. Deswegen streiken wir nicht nur, sondern bilden uns alle weiter! Auf YouTube gibt es tolle Webinare rund um das Thema Klimagerechtigkeit. Bilde dich mit uns zusammen und schaue dir ein Video auf unserem YouTube-Kanal an!',
+              Text('In der Schule wird nicht ausreichend über die Klimakrise unterrichtet. Deswegen streiken wir nicht nur, sondern bilden uns alle weiter! Auf YouTube gibt es tolle Webinare rund um das Thema Klimagerechtigkeit. Bilde dich mit uns zusammen und schaue dir ein Video auf unserem YouTube-Kanal an!'),
             ),
             _beeChallenge(
               'c1',
               'Der Picasso in dir!',
-              'Überlege dir eine Kunstaktion, ob Bild, Knete, Basteln oder Lego mit der du dein Klimathema repräsentierst. Ob klimaneutrales Haus, demonstrierende Menschen, ein Windrad oder eine Kuh, deiner Kreativität sind keine Grenzen gesetzt.',
+              Text('Überlege dir eine Kunstaktion, ob Bild, Knete, Basteln oder Lego mit der du dein Klimathema repräsentierst. Ob klimaneutrales Haus, demonstrierende Menschen, ein Windrad oder eine Kuh, deiner Kreativität sind keine Grenzen gesetzt.'),
             ),
             _beeChallenge(
               'c10',
               'InstagramStory für die Zukunft',
-              'Hast du schon die Challenge “Schreib deine eigene Zukunfts-Story” unter NetzStreik ausprobiert? Lasst uns verbreiten, wie wir unsere Zukunft sehen!',
+              Text('Hast du schon die Challenge “Schreib deine eigene Zukunfts-Story” unter NetzStreik ausprobiert? Lasst uns verbreiten, wie wir unsere Zukunft sehen!'),
             ),
             _beeChallenge(
               'c6',
               'Challenge & More',
-              'Erfinde deine eigene Challenge und poste sie unter #appchallengeforfuture oder #challengeforfuture. Sei dabei möglichst kreativ und denk dabei an die Prinzipien von FFF. Diese kannst du auch nochmal in der App nachlesen!',
+              Text('Erfinde deine eigene Challenge und poste sie unter #appchallengeforfuture oder #challengeforfuture. Sei dabei möglichst kreativ und denk dabei an die Prinzipien von FFF. Diese kannst du auch nochmal in der App nachlesen!'),
             ),
           ],
         ),
@@ -55,7 +55,7 @@ class _State extends State<ChallengePage> {
     );
   }
 
-  Widget _beeChallenge(String id, String title, String text) {
+  Widget _beeChallenge(String id, String title, Widget text) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -91,7 +91,7 @@ class _State extends State<ChallengePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
-                    Text(text),
+                    text,
                     if (!(Hive.box('challenges').get(id) ?? false))
                       RaisedButton(
                           color: Theme.of(context).primaryColor,
