48728871966eb290f0a81c3437301df007abd77c
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-05-31 11:18:52 +0200

    add plz form

--- lib/page/strike/mail_strike.dart
+++ lib/page/strike/mail_strike.dart
@@ -1,15 +1,34 @@
 import 'package:app/app.dart';
 import 'package:flutter/rendering.dart';
 
-class MailStrikePage extends StatelessWidget {
+class MailStrikePage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MailStrikePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _submitButton() {
+    if (_formKey.currentState.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+
+      // TODO
+      Scaffold.of(context)
+          .showSnackBar(SnackBar(content: Text('Processing Data')));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bailout Mail Aktion'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           /* Image.network(
             'https://app.fffutu.re/img/instagram_instructions.jpg',
@@ -24,7 +43,34 @@ class MailStrikePage extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
-          // PLZ Input
+          Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'PLZ eingeben',
+                      border: OutlineInputBorder(),
+                    ),
+
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'PLZ nicht gefunden';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: _submitButton,
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
