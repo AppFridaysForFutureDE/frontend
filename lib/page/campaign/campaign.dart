0fd7d8fefcd3ad1f6ee60f172382d65a0a43fbb6
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-09-05 11:56:30 +0200

    display campaign grid content from api

--- lib/page/campaign/campaign.dart
+++ lib/page/campaign/campaign.dart
@@ -49,39 +49,45 @@ class _CampaignPageState extends State<CampaignPage> {
       appBar: AppBar(
         title: Text('Aktionen'),
       ),
-      body: ListView(
-        children: <Widget>[
-          CarouselSlider(
-            options: CarouselOptions(height: 400.0),
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(color: Colors.amber),
-                      child: Text(
-                        'text $i',
-                        style: TextStyle(fontSize: 16.0),
-                      ));
-                },
-              );
-            }).toList(),
-          ),
-          GridView.count(
-            crossAxisCount: 2,
-            physics: ScrollPhysics(),
-            shrinkWrap: true,
-            children: List.generate(20, (index) {
-              return Center(
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              );
-            }),
-          ),
-        ],
+      body: RefreshIndicator(
+        onRefresh: _loadData,
+        child: campaigns.isEmpty
+            ? Center(
+                child: Text('Keine Ergebnisse'),
+              )
+            : ListView(
+                children: <Widget>[
+                  CarouselSlider(
+                    options: CarouselOptions(height: 400.0),
+                    items: [1, 2, 3, 4, 5].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(color: Colors.amber),
+                              child: Text(
+                                'text $i',
+                                style: TextStyle(fontSize: 16.0),
+                              ));
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  GridView.count(
+                    crossAxisCount: 2,
+                    physics: ScrollPhysics(),
+                    shrinkWrap: true,
+                    children: List.generate(campaigns.length, (index) {
+                      return Center(
+                        child: Text(
+                          campaigns[index].text,
+                        ),
+                      );
+                    }),
+                  ),
+                ],
+              ),
       ),
     );
   }
