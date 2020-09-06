2cf2026fa7f04a661ca230ee168c376af3cb945d
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-09-06 11:49:13 +0200

    quick fix campaigns page top image

--- lib/page/campaign/campaign.dart
+++ lib/page/campaign/campaign.dart
@@ -57,23 +57,25 @@ class _CampaignPageState extends State<CampaignPage> {
               )
             : ListView(
                 children: <Widget>[
-                  CarouselSlider(
-                    options: CarouselOptions(height: 400.0),
-                    items: [1, 2, 3, 4, 5].map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                              width: MediaQuery.of(context).size.width,
-                              margin: EdgeInsets.symmetric(horizontal: 5.0),
-                              decoration: BoxDecoration(color: Colors.amber),
-                              child: Text(
-                                'text $i',
-                                style: TextStyle(fontSize: 16.0),
-                              ));
-                        },
-                      );
-                    }).toList(),
-                  ),
+                  Image.network('https://fridaysforfuture.de/wp-content/uploads/2020/08/cropped-header.jpg'),
+                  // TODO: Load banners from api and fix layout
+                  // CarouselSlider(
+                  //   options: CarouselOptions(height: 400.0),
+                  //   items: [1, 2, 3, 4, 5].map((i) {
+                  //     return Builder(
+                  //       builder: (BuildContext context) {
+                  //         return Container(
+                  //             width: MediaQuery.of(context).size.width,
+                  //             margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  //             decoration: BoxDecoration(color: Colors.amber),
+                  //             child: Text(
+                  //               'text $i',
+                  //               style: TextStyle(fontSize: 16.0),
+                  //             ));
+                  //       },
+                  //     );
+                  //   }).toList(),
+                  // ),
                   GridView.count(
                     crossAxisCount: 2,
                     physics: ScrollPhysics(),
