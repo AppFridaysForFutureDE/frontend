0e3a503832f73c775deaf04c15997ea0c602e2d9
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-09-12 11:36:50 +0200

    improve og info page
    add title
    use og model fields

--- lib/widget/og_tile.dart
+++ lib/widget/og_tile.dart
@@ -51,11 +51,16 @@ class _OgTileState extends State<OgTile> {
 
   @override
   Widget build(BuildContext context) {
-    final ogImage = Image.network(
-        'https://www.codemate.com/wp-content/uploads/2017/09/flutter-logo.png');
-    final ogTitle = 'Mein super Titel';
-    final ogText =
+    // TODO: Remove
+    og.imageLink =
+        'https://www.zooroyal.de/magazin/wp-content/uploads/2017/11/Kakadu-760x560.jpg';
+    // 'https://www.codemate.com/wp-content/uploads/2017/09/flutter-logo.png');
+    og.infoTitle = 'So bereiten wir uns auf dem Großstreik in München vor';
+    og.infoText =
         'Sobald das Datum des Streiks steht, geht die Planung los: In einer ersten Telefonkonferenz, kurz TK, wurden sowohl 14 Uhr als Uhrzeit und Theresienwiese als Ort, wie auch die Aktionsform festgelegt. Außerdem wurden erste Ideen und Pläne für die Arbeitsweise und vorläufige Zeitpläne erstellt. Kurz nach der zweiten Telefonkonferenz stand als Arbeitsweise das Arbeiten in themenspezifischen Kleingruppen fest. So gibt es unter anderem Gruppen für Presse, Programm und Logistik. Ergebnisse der Arbeit in diesen Untergruppen, kurz UGs, werden in wöchentlichen Plena besprochen und abgesegnet. Außerdem hat jede UG mindestens einen Hutmenschen, der*die sich darum kümmert, dass die UG mit der Arbeit vorankommt, TKs stattfinden und als Ansprechpartner*in zur Verfügung steht.';
+
+    final ogImage = Image.network(og.imageLink);
+
     return ExpansionTile(
       initiallyExpanded: true,
       title: Padding(
@@ -78,6 +83,7 @@ class _OgTileState extends State<OgTile> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              // TODO: Display dates in title of ExpansionTile
               Row(
                 children: [
                   Text(
@@ -131,11 +137,16 @@ class _OgTileState extends State<OgTile> {
               //               onPressed: () {
               //                 _launchURL(strike.eventLink);
               //               }))
-              Text(ogTitle, style: TextStyle(fontSize: 16)),
+              // SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Text(og.infoTitle, style: TextStyle(fontSize: 20)),
+              ),
 
               DropCapText(
-                ogText,
+                og.infoText,
                 dropCap: DropCap(
+                    // TODO: fix image size
                     width: ogImage.width != null ? ogImage.width : 100,
                     height: ogImage.height != null ? ogImage.height : 100,
                     child: ogImage),
